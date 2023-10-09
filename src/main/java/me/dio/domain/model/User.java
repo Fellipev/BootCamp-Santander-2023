78d826e04fa6aaa2cce52bffbe9d7459b4d0f287
Fellisr <fellipe.reis@tlv.ag>
2023-10-09 19:23:21 -0300

    implementacao da camada de dominio
    Signed-off-by: Fellisr <fellipe.reis@tlv.ag>

---
+++ src/main/java/me/dio/domain/model/User.java
@@ -0,0 +1,62 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Account acount;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Card card;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Feature> features;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<News> news;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Account getAcount() {
+        return acount;
+    }
+
+    public void setAcount(Account acount) {
+        this.acount = acount;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public List<Feature> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(List<Feature> features) {
+        this.features = features;
+    }
+
+    public List<News> getNews() {
+        return news;
+    }
+
+    public void setNews(List<News> news) {
+        this.news = news;
+    }
+}
